40c2b8b55a855cd84217d3f945bf1a5e4fe9568d
Dominik Widomski <dominik@digital-detox.co.uk>
2017-05-08 11:49:43 +0100

    Update empty titles identifier

--- ids/emptyTitles.js
+++ ids/emptyTitles.js
@@ -4,5 +4,11 @@ const chalk = require('chalk');
  * check if title is empty on page
  */
 module.exports = function emptyTitles(document) {
-	console.log(chalk.cyan(`Page title: ${document.querySelector('title').innerHTML}`));
+	const title = document.querySelector('title').innerHTML;
+	if(!title) {
+		console.log(chalk.bgRed.black(`No title`));
+	} else {
+		console.log(chalk.bgCyan.black(`Page title: ${title}`));
+	}
+
 }
